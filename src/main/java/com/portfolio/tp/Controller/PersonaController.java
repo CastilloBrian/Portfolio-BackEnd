ea2cd3aea419ec53273c38fee115c6615c8939d6
CastilloBrian <briancastilloisei@yahoo.com>
2023-05-13 03:42:41 -0300

    Update PersonaController.java

--- src/main/java/com/portfolio/tp/Controller/PersonaController.java
+++ src/main/java/com/portfolio/tp/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.tp.Entity.Persona;
 import com.portfolio.tp.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +15,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService iPersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> GetPersona(){
         return iPersonaService.getPersona();
     }
@@ -46,5 +48,10 @@ public class PersonaController {
         
         iPersonaService.savePersona(persona);
         return persona;
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return iPersonaService.findPersona((long)1);
     }
 }
